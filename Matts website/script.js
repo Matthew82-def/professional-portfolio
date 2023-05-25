fcbe686a2920df707a8ae6711c05cfdf5a83fc75
Matthew82-def <matt.herrera82@gmail.com>
2023-05-24 23:58:07 -0500

    Initial Commit

--- Matts website/script.js
+++ Matts website/script.js
@@ -19,21 +19,34 @@ $(document).ready(function() {
 
 
 
-    let gitHubRequest = new XMLHttpRequest();
-    gitHubRequest.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            let gitObject = JSON.parse(this.responseText);
-            for (var i = 0; i < gitObject.length; i++){
-                //jQuery append
-                $("#gitHub").append("<p>" + gitObject[i].name + "</p>");
-            }
-            // myObject[i].name
-            // document.getElementById("gitHub").innerHTML = gitObject.name;
-            console.log(gitObject.hub);
+//     let gitHubRequest = new XMLHttpRequest();
+//     gitHubRequest.onreadystatechange = function () {
+//         if (this.readyState == 4 && this.status == 200) {
+//             let gitObject = JSON.parse(this.responseText);
+//             for (var i = 0; i < gitObject.length; i++){
+//                 //jQuery append
+//                 $("#gitHub").append("<p>" + gitObject[i].name + "</p>");
+//             }
+//             // myObject[i].name
+//             // document.getElementById("gitHub").innerHTML = gitObject.name;
+//             console.log(gitObject.hub);
+//         }
+//     };
+// gitHubRequest.open("GET", "https://api.github.com/users/MATTHEW82-def/repos", true);
+//     gitHubRequest.send();
+
+
+function loadRepo() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        results = JSON.parse(this.responseText);
+        for (let i = 0; i < results.length; i++) {
+          console.log(results[i].name);
+          $("#repositories").append("<li>" + results[i].name + "</li>");
         }
-    };
-gitHubRequest.open("GET", "https://api.github.com/users/MATTHEW82-def/repos", true);
-    gitHubRequest.send();
-
-
-    
+    }
+  };
+  xhttp.open("GET", "https://api.github.com/users/MATTHEW82-def/repos", true);
+  xhttp.send();
+}
