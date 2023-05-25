e7379c159cb88729b4696958c244ee3ecad39e19
Matthew82-def <matt.herrera82@gmail.com>
2023-05-25 13:32:51 -0500

    ajax var implemented

--- script.js
+++ script.js
@@ -7,28 +7,20 @@ $(document).ready(function() {
 });
 
 
-
-
-// function loadRepo() {
-//     const xhttp = new XMLHttpRequest();
-//     xhttp.onreadystatechange = function() {
-//       if (this.readyState == 4 && this.status == 200) {
-//         results = JSON.parse(this.responseText);
-//         for (let i = 0; i < results.length; i++) {
-//           console.log(results[i].name);
-//           $("#repositories").append("<li>" + results[i].name + "</li>");
-//         }
-//     }
-//   };
+let hasLoaded = false;
 
 function loadRepo() {
+  if (hasLoaded) {
+    return; // Exit the method if it has already been executed
+  }
+
   const xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function () {
     if (this.readyState === 4 && this.status === 200) {
       results = JSON.parse(this.responseText);
       for (let i = 0; i < results.length; i++) {
         console.log(results[i].name);
-        let ul = document.getElementById("repositories");
+        let ul = document.getElementById("repo1");
         let li = document.createElement("LI");
         li.className = "list-group-items";
         let a = document.createElement("a");
@@ -38,8 +30,11 @@ function loadRepo() {
         a.setAttribute("href", results[i].html_url);
         ul.appendChild(a);
       }
+
+      hasLoaded = true;
     }
   };
-  xhttp.open("GET", "https://api.github.com/users/MATTHEW82-def/repos", true);
+
+  xhttp.open("GET", "https://api.github.com/users/Matthew82-def/repos", true);
   xhttp.send();
 }
