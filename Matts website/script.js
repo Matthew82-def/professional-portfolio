95e14e791161bcd74862686da7c946ca04819f63
Matthew82-def <matt.herrera82@gmail.com>
2023-05-25 13:02:45 -0500

    Updated ajax to fetch repos

--- Matts website/script.js
+++ Matts website/script.js
@@ -6,45 +6,38 @@ $(document).ready(function() {
     });
 });
 
-// 1em = 16px
-// Each icon = 64px
-
-// $(document).ready(function() {
-//     $("#header").hover(function() {
-//         $(this).css("background-color" , "silver" );   
-//     }, function(){
-//         $(this).css("background-color" , "yellow" );
-//     });
-// });
 
 
 
-//     let gitHubRequest = new XMLHttpRequest();
-//     gitHubRequest.onreadystatechange = function () {
-//         if (this.readyState == 4 && this.status == 200) {
-//             let gitObject = JSON.parse(this.responseText);
-//             for (var i = 0; i < gitObject.length; i++){
-//                 //jQuery append
-//                 $("#gitHub").append("<p>" + gitObject[i].name + "</p>");
-//             }
-//             // myObject[i].name
-//             // document.getElementById("gitHub").innerHTML = gitObject.name;
-//             console.log(gitObject.hub);
+// function loadRepo() {
+//     const xhttp = new XMLHttpRequest();
+//     xhttp.onreadystatechange = function() {
+//       if (this.readyState == 4 && this.status == 200) {
+//         results = JSON.parse(this.responseText);
+//         for (let i = 0; i < results.length; i++) {
+//           console.log(results[i].name);
+//           $("#repositories").append("<li>" + results[i].name + "</li>");
 //         }
-//     };
-// gitHubRequest.open("GET", "https://api.github.com/users/MATTHEW82-def/repos", true);
-//     gitHubRequest.send();
-
+//     }
+//   };
 
 function loadRepo() {
-    const xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        results = JSON.parse(this.responseText);
-        for (let i = 0; i < results.length; i++) {
-          console.log(results[i].name);
-          $("#repositories").append("<li>" + results[i].name + "</li>");
-        }
+  const xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+      results = JSON.parse(this.responseText);
+      for (let i = 0; i < results.length; i++) {
+        console.log(results[i].name);
+        let ul = document.getElementById("repositories");
+        let li = document.createElement("LI");
+        li.className = "list-group-items";
+        let a = document.createElement("a");
+
+        li.appendChild(document.createTextNode(results[i].name));
+        a.appendChild(li);
+        a.setAttribute("href", results[i].html_url);
+        ul.appendChild(a);
+      }
     }
   };
   xhttp.open("GET", "https://api.github.com/users/MATTHEW82-def/repos", true);
